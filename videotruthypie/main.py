d706563a4f0b11177319325ce7b76713290f7832
Dale Inverarity <dale@diit.ca>
2021-11-21 23:59:33 -0500

    4 hotkeys, glob syntax, single file output

--- videotruthypie/main.py
+++ videotruthypie/main.py
@@ -1,25 +1,23 @@
 import numpy as np
+import argparse
+import glob
 import cv2
 import os
-
-# Where the stuff is stored
-ASSETS_DIRECTORY = 'assets'
-OUTPUT_DIRECTORY = 'output'
 
 # Because mac is doggy doo doo
 NAUTY_WORDS = ['.DS_Store']
 
-def play_video(folder, file):
-    print("Playing:", folder, file)
+def play_video(file):
+    print("Playing:", file)
+
+    # Setup our data
+    videoData = np.zeros((0,), dtype=int)
 
     # load video capture from file
-    video = cv2.VideoCapture(os.path.join(folder, file))
+    video = cv2.VideoCapture(file)
 
     # window name and size
     cv2.namedWindow("video", cv2.WINDOW_AUTOSIZE)
-
-    # Setup our data
-    data = np.zeros((0,), dtype=int)
 
     # See what we're getting into
     frameCount = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -39,20 +37,28 @@ def play_video(folder, file):
         
         # show one frame at a time
         key = cv2.waitKey(0)
-        while key not in [ord('q'), ord('k'), ord('l')]:
+        while key not in [ord('q'), ord('h'), ord('j'), ord('k'), ord('l')]:
             key = cv2.waitKey(0)
 
         # Quit when 'q' is pressed
         if key == ord('q'):
             break
 
-        # Record a "bad frame" when K is pressed
-        if key == ord('k'):
-            data = np.append(data, [0])
+        # Good Frame
+        if key == ord('h'):
+            videoData = np.append(videoData, [0])
         
-        # Record a "good frame" when L is pressed
+        # Bad Frame
+        if key == ord('j'):
+            videoData = np.append(videoData, [1])
+
+        # Auto-Dead Frame (for matching when we auto remove a frame)
+        if key == ord('k'):
+            videoData = np.append(videoData, [8])
+
+        # Dead Frame
         if key == ord('l'):
-            data = np.append(data, [1])
+            videoData = np.append(videoData, [9])
 
     # Release capture object
     video.release()
@@ -60,21 +66,45 @@ def play_video(folder, file):
     # Exit and distroy all windows
     cv2.destroyAllWindows()
 
-    # Show the beautiful data
-    print(data)
-
-    # Save the beautiful data
-    np.savetxt(os.path.join(OUTPUT_DIRECTORY, file + ".csv"), data, fmt='%i', delimiter=",")
+    # Show and return the beautiful data
+    print(videoData)
+    return videoData
 
 def start():
-    print("Hello Kayla!\n\n")
+    # Get options from user
+    parser = argparse.ArgumentParser(description='Kaylas Magic Video Truthy Pie, made fresh every morning with love!')
+    parser.add_argument('assets', type=str, help='Glob pattern for assets location')
+    parser.add_argument('--output', dest='output', default="./output.csv", help='where to save data (default: ./output.csv)')
+    args = parser.parse_args()
 
-    # We want to loop over all videos in the assets folder
-    filenames = next(os.walk(ASSETS_DIRECTORY), (None, None, []))[2]
+    # Find all files in glob path
+    filenames = glob.glob(args.assets)
 
     # Remove garbage
     safeFilenames = [word for word in filenames if word not in NAUTY_WORDS]
 
+    # Setup our data
+    data = []
+
     # Loop over each and play the video
     for path in safeFilenames:
-        play_video(ASSETS_DIRECTORY, path)
+        videoData = play_video(path)
+        data.append(videoData.tolist())
+
+    # We determine the max length of frames in any of the videos
+    rowLengths = []
+    for row in data:
+        rowLengths.append(len(row))
+    maxLength = max(rowLengths)
+
+    # Then "pad" the remaining data so that we have a "balanced" matrix
+    for row in data:
+        while len(row) < maxLength:
+            row.append('')
+    balancedArray = np.array(data)
+
+    # Transpose so that each column is a video
+    finalData = balancedArray.transpose()
+
+    # Save the beautiful data
+    np.savetxt(args.output, finalData, fmt="%s", delimiter=",")
